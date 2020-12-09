3740a1cd331c1437f678c8a8e9d5749b487aa75e
Stepan <stepangovorov5911@gmail.com>
2020-12-09 10:22:09 +0300

    Все работает

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1160px;
+  width: 100%;
+  max-height: 870px;
+}
+</style>
